d49ac43003c3baf23cf52a083ee14c444a6c444b
Экгард Викентий Александрович <EkgartVA@main.sgu.ru>
2018-09-25 13:35:39 +0400

    added interpolation points

--- task2.js
+++ task2.js
@@ -1,16 +1,30 @@
 const POINTLEFT = -0.5;
 const POINTRIGHT = 0.5;
+const STEP = 0.1
+const DEBUG = true;
 const originalFunct = (x) => {
     return 1/Math.sqrt(1 - Math.pow(x,2));
 }
 
 const fX = [];
+const pointsArray = [];
 let currentPoint = POINTLEFT;
 while (currentPoint <= POINTRIGHT){
     const result = originalFunct(currentPoint);
-    console.log(` ${currentPoint.toFixed(1)}  ${result}`);
+    DEBUG && console.log(` ${currentPoint.toFixed(1)}  ${result}`);
     fX.push(result);
-    currentPoint += 0.1;
+    pointsArray.push(currentPoint);
+    currentPoint += STEP;
 }
 
+
+const pointsArrayForInterpolation = [];
+for (let i = 1; i < pointsArray.length; i++ ){
+    const point = (pointsArray[i-1] + pointsArray[i])/2;
+    pointsArrayForInterpolation.push(point);
+}
 console.log(fX);
+console.log(pointsArrayForInterpolation);
+
+
+
